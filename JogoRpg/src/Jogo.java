5483d62e51e80343280bbe7e7daa2e4fdc1b0fb6
Natanael Alves <natanaelpereiraalves@gmail.com>
2025-02-26 14:33:28 -0300

    Personagem

---
+++ JogoRpg/src/Jogo.java
@@ -0,0 +1,32 @@
+public class Jogo {
+    public static void main(String[] args) throws Exception {
+     
+        Personagem p1 = new Personagem("A", "Mago", 10, 5, 2);
+   
+   
+
+        p1.informacaoPersonagem();
+
+        int dano = p1.danoAleatorio();
+        System.out.println("Dano: " + dano);
+
+        boolean exit = false;
+
+        do {
+
+            
+        }while (!exit);
+         
+            
+
+
+        
+   
+
+   
+   
+   
+   
+   
+    }
+}
---
+++ JogoRpg/src/Personagem.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+
+public class Personagem {
+    
+      Random teclado = new Random();
+
+   
+    //Criação dos Personagem
+    private final String personagem;
+    private final String classeDoPersonagem;
+    private final int vidaPersonagem;
+    private final int danoMax;
+    private final int danoMin;
+
+    //Metado para simplicificar a criação dos personagem 
+    public Personagem(String personagem, String classeDoPersonagem, int vidaPersonagem, int danoMax, int danoMin){
+        this.personagem = personagem;
+        this.classeDoPersonagem = classeDoPersonagem;
+        this.vidaPersonagem = vidaPersonagem;
+        this.danoMax = danoMax;
+        this.danoMin = danoMin;
+
+    }
+
+    //Informação sobre os Personagem
+    public void informacaoPersonagem(){
+        System.out.println("Nome: " + personagem);
+        System.out.println("Classe: " + classeDoPersonagem);
+        System.out.println("Vida: " + vidaPersonagem);
+        System.out.println("Dano Maximo: " + danoMax);
+        System.out.println("Dano Minino: " + danoMin);
+    }
+
+    
+   
+    public int danoAleatorio() {
+    
+        return teclado.nextInt(this.danoMax - this.danoMin + 1) + danoMin;
+    }
+
+
+
+    
+}
